ec0d006031463b382be43c620e0ff980de0195ae
dhoppe <dhoppe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-01 23:59:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fancy-gallery/trunk@513254 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- media-upload-gallery-tab.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($){
-  
-  // Hide the senseless gallery setting fields
-  $('table#basic tr:eq(0), table#basic tr:eq(3)').hide();
-  
-  // Add Fancy Gallery Pro Banner
-  $('table#basic').before( $('div#fancy-gallery-banner') );
-  
-});
